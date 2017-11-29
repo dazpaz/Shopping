2d7bb732cd1f1de49865498f7df90e52f267db68
Daz Paz <darren.powell@hotmail.co.uk>
2017-11-29 14:13:02 +0000

    Add User Actor

---
+++ UserActor/UserActor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Runtime;
+using UserActor.Interfaces;
+
+namespace UserActor
+{
+	[StatePersistence(StatePersistence.Persisted)]
+	internal class UserActor : Actor, IUserActor
+	{
+		public UserActor(ActorService actorService, ActorId actorId)
+			: base(actorService, actorId)
+		{
+		}
+
+		public async Task AddToBasket(Guid productId, int quantity)
+		{
+			await StateManager.AddOrUpdateStateAsync(productId.ToString(),
+				quantity,
+				(id, oldQuantity) => oldQuantity + quantity);
+		}
+
+		public async Task ClearBasket()
+		{
+			IEnumerable<string> productIDs = await StateManager.GetStateNamesAsync();
+
+			foreach (string productId in productIDs)
+			{
+				await StateManager.RemoveStateAsync(productId);
+			}
+		}
+
+		public async Task<Dictionary<Guid, int>> GetBasket()
+		{
+			var result = new Dictionary<Guid, int>();
+
+			IEnumerable<string> productIDs = await StateManager.GetStateNamesAsync();
+
+			foreach (string productId in productIDs)
+			{
+				int quantity = await StateManager.GetStateAsync<int>(productId);
+				result[new Guid(productId)] = quantity;
+			}
+
+			return result;
+		}
+	}
+}
