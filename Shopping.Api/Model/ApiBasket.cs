0840364b92833134215dba0a6c19789bab9c1763
Daz Paz <darren.powell@hotmail.co.uk>
2017-11-29 14:46:29 +0000

    Add Basket Controller to the API

---
+++ Shopping.Api/Model/ApiBasket.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Shopping.Api.Model
+{
+	public class ApiBasket
+	{
+		[JsonProperty("userId")]
+		public string UserId { get; set; }
+
+		[JsonProperty("item")]
+		public ApiPBasketItem[] Items { get; set; }
+	}
+
+	public class ApiPBasketItem
+	{
+		[JsonProperty("productId")]
+		public string ProductId { get; set; }
+
+		[JsonProperty("quantity")]
+		public int Quantity { get; set; }
+	}
+}
---
+++ Shopping.Api/Model/ApiBasketAddRequest.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Shopping.Api.Model
+{
+	public class ApiBasketAddRequest
+	{
+		[JsonProperty("productId")]
+		public Guid ProductId { get; set; }
+
+		[JsonProperty("quantity")]
+		public int Quantity { get; set; }
+	}
+}
